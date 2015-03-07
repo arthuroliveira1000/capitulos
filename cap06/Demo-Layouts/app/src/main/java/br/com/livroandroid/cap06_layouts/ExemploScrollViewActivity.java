de8f50b89d59ce87f1d36666b5c3ef76af164b9e
livroandroidup@gmail.com <livroandroidup@gmail.com>
2015-03-07 15:02:30 -0300

    OK

---
+++ cap06/Demo-Layouts/app/src/main/java/br/com/livroandroid/cap06_layouts/ExemploScrollViewActivity.java
@@ -0,0 +1,29 @@
+package br.com.livroandroid.cap06_layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ExemploScrollViewActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		setContentView(R.layout.activity_exemplo_scrollview);
+		LinearLayout layout = (LinearLayout) findViewById(R.id.layout1);
+		for (int i = 0; i < 100; i++) {
+
+			// Instancia o TextView inflando o arquivo de layout
+			TextView text = (TextView) LayoutInflater.from(this).inflate(R.layout.inflate_textview, layout, false);
+
+			// Instancia o TextView pela API
+//			TextView text = new TextView(this);
+//			text.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+
+			text.setText("Texto: " + i);
+			layout.addView(text);
+		}
+	}
+}
