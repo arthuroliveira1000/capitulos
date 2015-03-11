45820e6c73fbf4fa73b403b86935f8f09167c521
livroandroidup@gmail.com <livroandroidup@gmail.com>
2015-03-11 16:12:14 -0300

    OK

--- cap24/HelloNotification/app/src/main/java/br/com/livroandroid/hellonotification/MainActivity.java
+++ cap24/HelloNotification/app/src/main/java/br/com/livroandroid/hellonotification/MainActivity.java
@@ -1,11 +1,27 @@
 package br.com.livroandroid.hellonotification;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
+
+    private BroadcastReceiver customActionReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "CLICOU NA AÇÂO!", Toast.LENGTH_SHORT).show();
+            NotificationUtil.cancell(context, 3);
+        }
+    };
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -13,16 +29,46 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(customActionReceiver, new IntentFilter(NotificationUtil.ACTION_VISUALIZAR));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(customActionReceiver);
+    }
 
     public void onClickNotificacaoSimples(View view) {
         int id = 1;
         String contentTitle = "Nova mensagem";
-        String contentText = "Você possui 5 novas mensagens";
+        String contentText = "Você possui 3 novas mensagens";
         Intent intent = new Intent(this,MensagemActivity.class);
         intent.putExtra("msg","Olá Leitor, como vai?");
-        NotificationUtil.create(this,intent,contentTitle,contentText,id);
+        NotificationUtil.create(this,intent,contentTitle,contentText,1);
     }
 
     public void onClickNotificacaoBig(View view) {
+        int id = 2;
+        String contentTitle = "Nova mensagem";
+        String contentText = "Você possui 3 novas mensagens";
+        Intent intent = new Intent(this,MensagemActivity.class);
+        intent.putExtra("msg","Olá Leitor, como vai?");
+        List<String> lines = new ArrayList<>();
+        lines.add("Mensagem 1");
+        lines.add("Mensagem 2");
+        lines.add("Mensagem 3");
+        NotificationUtil.createBig(this, intent, contentTitle, contentText, lines, id);
+    }
+
+    public void onClickNotificacaoComAcao(View view) {
+        int id = 3;
+        String contentTitle = "Nova mensagem";
+        String contentText = "Você possui 3 novas mensagens";
+        Intent intent = new Intent(this,MensagemActivity.class);
+        intent.putExtra("msg","Olá Leitor, como vai?");
+        NotificationUtil.createWithAction(this, intent, contentTitle, contentText, 1);
     }
 }
--- cap24/HelloNotification/app/src/main/java/br/com/livroandroid/hellonotification/NotificationUtil.java
+++ cap24/HelloNotification/app/src/main/java/br/com/livroandroid/hellonotification/NotificationUtil.java
@@ -8,10 +8,16 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.TaskStackBuilder;
 
+import java.util.List;
+
 /**
  * Classe utilitária para criar notificações.
+ *
+ * http://developer.android.com/guide/topics/ui/notifiers/notifications.html
  */
 public class NotificationUtil {
+
+    public static final String ACTION_VISUALIZAR = "br.com.livroandroid.hellonotification.ACTION_VISUALIZAR";
 
     private static PendingIntent getPendingIntent(Context context, Intent intent, int id) {
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
@@ -24,8 +30,8 @@ public class NotificationUtil {
     public static void create(Context context, Intent intent, String contentTitle, String contentText, int id) {
         PendingIntent p = getPendingIntent(context, intent, id);
 
+        // Cria a notificação
         NotificationCompat.Builder b = new NotificationCompat.Builder(context);
-
         b.setDefaults(Notification.DEFAULT_ALL); // Ativa configurações padrão
         b.setSmallIcon(R.mipmap.ic_launcher); // Ícone
         b.setContentTitle(contentTitle); // Título
@@ -36,6 +42,93 @@ public class NotificationUtil {
         NotificationManagerCompat nm = NotificationManagerCompat.from(context);
         nm.notify(id, b.build());
     }
+
+    public static void createBig(Context context, Intent intent, String contentTitle, String contentText,List<String> lines, int id) {
+        PendingIntent p = getPendingIntent(context, intent, id);
+
+        // Configura o estilo Inbox
+        int size = lines.size();
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle(contentTitle);
+        for (String s: lines) {
+            inboxStyle.addLine(s);
+        }
+        inboxStyle.setSummaryText(contentText);
+
+        // Cria a notificação
+        NotificationCompat.Builder b = new NotificationCompat.Builder(context);
+        b.setDefaults(Notification.DEFAULT_ALL); // Ativa configurações padrão
+        b.setSmallIcon(R.mipmap.ic_launcher); // Ícone
+        b.setContentTitle(contentTitle); // Título
+        b.setContentText(contentText); // Mensagem
+        b.setContentIntent(p); // Intent que será chamada ao clicar na notificação.
+        b.setAutoCancel(true); // Auto cancela a notificação ao clicar nela
+        b.setNumber(size); // Número para aparecer na notificação
+        b.setStyle(inboxStyle); // Estilo customizado
+
+        NotificationManagerCompat nm = NotificationManagerCompat.from(context);
+        nm.notify(id, b.build());
+    }
+
+    public static void createWithAction(Context context, Intent intent, String contentTitle, String contentText, int id) {
+        PendingIntent p = getPendingIntent(context, intent, id);
+
+        PendingIntent actionIntent = PendingIntent.getBroadcast(
+                context, 0, new Intent(ACTION_VISUALIZAR), 0);
+
+        // Cria a notificação
+        NotificationCompat.Builder b = new NotificationCompat.Builder(context);
+        b.setDefaults(Notification.DEFAULT_ALL); // Ativa configurações padrão
+        b.setSmallIcon(R.mipmap.ic_launcher); // Ícone
+        b.setContentTitle(contentTitle); // Título
+        b.setContentText(contentText); // Mensagem
+        b.setContentIntent(p); // Intent que será chamada ao clicar na notificação.
+        b.setAutoCancel(true); // Auto cancela a notificação ao clicar nela
+
+        // Ação customizada
+        b.addAction(R.mipmap.ic_launcher, "Ação Customizada", actionIntent);
+
+        NotificationManagerCompat nm = NotificationManagerCompat.from(context);
+        nm.notify(id, b.build());
+    }
+
+    /*
+public static void criarNotificacaoCompleta(Context ctx, String texto, int id) {
+        Uri uriSom = Uri.parse("android.resource://"+
+                ctx.getPackageName() +"/raw/som_notificacao");
+
+        PendingIntent pitAcao = PendingIntent.getBroadcast(
+                ctx, 0, new Intent(ACAO_NOTIFICACAO), 0);
+        PendingIntent pitDelete = PendingIntent.getBroadcast(
+                ctx, 0, new Intent(ACAO_DELETE), 0);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(
+                ctx.getResources(), R.drawable.ic_launcher);
+
+        PendingIntent pitNotificacao= criarPendingIntent(ctx, texto, id);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(ctx)
+                        .setSmallIcon(R.drawable.ic_notificacao)
+                        .setContentTitle("Completa")
+                        .setContentText(texto)
+                        .setTicker("Chegou uma notificação")
+                        .setWhen(System.currentTimeMillis())
+                        .setLargeIcon(largeIcon)
+                        .setAutoCancel(true)
+                        .setContentIntent(pitNotificacao)
+                        .setDeleteIntent(pitDelete)
+                        .setLights(Color.BLUE, 1000, 5000)
+                        .setSound(uriSom)
+                        .setVibrate(new long[]{100, 500, 200, 800})
+                        .addAction(R.drawable.ic_acao_notificacao, "Ação Customizada", pitAcao)
+                        .setNumber(id)
+                        .setSubText("Subtexto");
+
+        NotificationManagerCompat nm = NotificationManagerCompat.from(ctx);
+        nm.notify(id, mBuilder.build());
+    }
+     */
 
     public static void cancell(Context context, int id) {
         NotificationManagerCompat nm = NotificationManagerCompat.from(context);
