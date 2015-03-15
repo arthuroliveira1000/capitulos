46478a404453779d216cf061c8f10cbff864f670
Ricardo <rlecheta@gmail.com>
2015-03-15 16:14:21 -0300

    First commit

--- cap21/PlayerMp3/app/src/main/java/br/com/livroandroid/playermp3/MainActivity.java
+++ cap21/PlayerMp3/app/src/main/java/br/com/livroandroid/playermp3/MainActivity.java
@@ -7,46 +7,36 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity {
     private static final String TAG = "livro";
     // Classe que encapsula o MediaPlayer
     private PlayerMp3 player = new PlayerMp3();
     private EditText text;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         setContentView(R.layout.activity_main);
-
         text = (EditText) findViewById(R.id.tArquivo);
+    }
 
-        findViewById(R.id.start).setOnClickListener(this);
-        findViewById(R.id.pause).setOnClickListener(this);
-        findViewById(R.id.stop).setOnClickListener(this);
-    }
-    /**
-     * @see android.view.View.OnClickListener#onClick(android.view.View)
-     */
-    public void onClick(View view) {
-        try {
-            if (view.getId() == R.id.start) {
-                String mp3 = text.getText().toString();
-                player.start(mp3);
-            } else if (view.getId() == R.id.pause) {
-                player.pause();
-            } else if (view.getId() == R.id.stop) {
-                player.stop();
-            }
-        } catch (Exception e) {
-            Log.e(TAG, e.getMessage(), e);
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // Libera recursos do MediaPlayer
         player.close();
+    }
+
+    public void onClickStart(View view) {
+        String mp3 = text.getText().toString();
+        player.start(mp3);
+    }
+
+    public void onClickPause(View view) {
+        player.pause();
+    }
+
+    public void onClickStop(View view) {
+        player.stop();
     }
 }
