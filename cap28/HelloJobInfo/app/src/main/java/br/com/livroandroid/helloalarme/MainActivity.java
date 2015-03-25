fad6e1e4a33e4c6071a4aca65968a4e415af738f
Ricardo <livroandroid@gmail.com>
2015-03-25 14:27:49 -0300

    First commit

---
+++ cap28/HelloJobInfo/app/src/main/java/br/com/livroandroid/helloalarme/MainActivity.java
@@ -0,0 +1,61 @@
+package br.com.livroandroid.helloalarme;
+
+import android.app.AlarmManager;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import static android.app.job.JobInfo.*;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Data/Tempo para agendar o alarme
+    public long getTime() {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(System.currentTimeMillis());
+        c.add(Calendar.SECOND, 5);
+        long time = c.getTimeInMillis();
+        return time;
+    }
+
+    public void onClickAgendar(View view) {
+        Intent intent = new Intent(LembremeDeComerReceiver.ACTION);
+
+        JobInfo uploadTask = new Builder(1,
+                mServiceComponent /* JobService component */)
+                .setRequiredNetworkCapabilities(NetworkType.UNMETERED)
+                .build();
+        JobScheduler jobScheduler =
+                (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.schedule(uploadTask);
+
+
+        Toast.makeText(this,"Job agendado.",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickAgendarComRepeat(View view) {
+        Intent intent = new Intent(LembremeDeComerReceiver.ACTION);
+        // Agenda para daqui a 5 seg, repete a cada 30 seg
+        AlarmUtil.scheduleRepeat(this, intent, getTime(), 30 * 1000);
+        Toast.makeText(this,"Alarme agendado com repetir.",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickCancelar(View view) {
+        Intent intent = new Intent(LembremeDeComerReceiver.ACTION);
+        AlarmUtil.cancel(this,intent);
+        Toast.makeText(this,"Alarme cancelado",Toast.LENGTH_SHORT).show();
+    }
+}
