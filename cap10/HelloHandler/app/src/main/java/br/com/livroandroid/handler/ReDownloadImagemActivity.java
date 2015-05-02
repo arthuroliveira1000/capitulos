41705058b0221298fdec0176e5f07a5345a58a10
Ricardo <livroandroid@gmail.com>
2015-05-02 01:19:55 -0300

    ok
    ok

--- cap10/HelloHandler/app/src/main/java/br/com/livroandroid/handler/ReDownloadImagemActivity.java
+++ cap10/HelloHandler/app/src/main/java/br/com/livroandroid/handler/ReDownloadImagemActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -22,16 +23,24 @@ public class ReDownloadImagemActivity extends AppCompatActivity {
     private static final String URL = "http://livroandroid.com.br/imgs/livro_android.png";
     private ProgressBar progress;
     private Handler handler = new Handler();
+    private ImageView imgView;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_download_imagem);
+        imgView = (ImageView) findViewById(R.id.img);
         progress = (ProgressBar) findViewById(R.id.progress);
-        progress.setVisibility(View.VISIBLE);
         downloadImagem();
     }
     // Faz o download da imagem em uma nova Thread
     private void downloadImagem() {
+        Toast.makeText(this,"Download!",Toast.LENGTH_SHORT).show();
+        // Zera a imagem para dar o efeito ao baixar novamente
+        imgView.setImageBitmap(null);
+        // Mostra o progress
+        progress.setVisibility(View.VISIBLE);
+        // Dispara a thread
         new Thread() {
             @Override
             public void run() {
@@ -41,12 +50,7 @@ public class ReDownloadImagemActivity extends AppCompatActivity {
                     // Atualiza a tela
                     atualizaImagem(bitmap);
                     // Agenda o download novamente (daqui a 10 seg)
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            downloadImagem();
-                        }
-                    },2000);
+                    handler.postDelayed(getRunnableDownload(),2000);
                 } catch (IOException e) {
                     // Uma aplicação real deveria tratar este erro
                     Log.e("Erro ao fazer o download: ", e.getMessage(), e);
@@ -54,6 +58,16 @@ public class ReDownloadImagemActivity extends AppCompatActivity {
             }
         }.start();
     }
+
+    private Runnable getRunnableDownload() {
+        return new Runnable() {
+            @Override
+            public void run() {
+                downloadImagem();
+            }
+        };
+    }
+
     // Vai dar Erro neste método pois somente a UI Thread pode atualizar a view
     private void atualizaImagem(final Bitmap imagem) {
        runOnUiThread(new Runnable() {
@@ -62,10 +76,16 @@ public class ReDownloadImagemActivity extends AppCompatActivity {
                // Esconde o progress
                progress.setVisibility(View.INVISIBLE);
                // Atualiza a imagem
-               ImageView imgView = (ImageView) findViewById(R.id.img);
                imgView.setImageBitmap(imagem);
            }
        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Cancela o runnable ao sair da activity
+        handler.removeCallbacksAndMessages(null);
     }
 }
 
